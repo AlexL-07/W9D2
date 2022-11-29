c1ec932cc23bb37bcc2aed2cef0e0141f0e0b7cd
bob-skywalker <q463250938@gmail.com>
2022-11-29 12:19:35 -0800

    finished phase1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 console.log("Webpack is working!")
+// import MovingObject from './moving_object';
+
+const MovingObject = require("./moving_object.js");
+
+window.MovingObject = MovingObject;
+// const mo = new MovingObject({
+//   pos: [30, 30],
+//   vel: [10, 10],
+//   radius: 50,
+//   color: "#00FF00"
+// });
+
+const canvas = document.getElementById("game-canvas")
+let context = canvas.getContext("2d")
+console.log(canvas)
+let new_moving_obj = new MovingObject({
+  pos: [30, 30],
+  vel: [10, 10],
+  radius: 50,
+  color: "#00FF00"
+});
+new_moving_obj.draw(context)
--- src/moving_object.js
+++ src/moving_object.js
@@ -1,13 +1,21 @@
-function MovingObject(){
-    
+function MovingObject(options){
+    this.pos = options["pos"]
+    this.vel = options["vel"]
+    this.radius = options["radius"]
+    this.color = options["color"]
 }
+
+
 MovingObject.prototype.move = function(){
 }
 
 MovingObject.prototype.draw = function(ctx){
-
+    ctx.fill();
+    ctx.fillStyle ="black";
+    ctx.arc(this.pos[0], this.pos[1], this.radius, 0, 2 * Math.PI, false);
 }
 
 MovingObject.prototype.isCollidedWith = function(otherMovingObject){
     
 }
+module.exports = MovingObject;
